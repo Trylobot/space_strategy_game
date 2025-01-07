d3e5a20b7b68671af5ea01bef1698ec42024ebda
Trylobot <trylobot@gmail.com>
2025-01-07 15:31:51 -0600

    starmap using nodes

---
+++ star.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+var idx = 0
+var size = 1.000
+var brightness = 1
+var distance_to_star = {}
+var stars_by_distance = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#position = Vector2.ZERO
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- starfield.gd
+++ starfield.gd
@@ -8,11 +8,10 @@ const STARLANE_COLOR = Color(0.5,0.5,0.5) # Grey lanes
 const MAP_WIDTH = 1024
 const MAP_HEIGHT = 768
 
-
-
-
 var default_font = ThemeDB.fallback_font
 var default_font_size = ThemeDB.fallback_font_size
+
+@onready var star_scene = load("res://star.tscn")
 
 var stars = []
 var star_lanes = []
@@ -21,48 +20,48 @@ func _ready():
 	randomize() # Seed the random number generator
 	generate_stars()
 	#update()
+	pass
 
 func _draw():
-	for s in star_lanes:
-		draw_line(stars[s.s0].position, stars[s.s1].position, STARLANE_COLOR, 0.25, true)
-	for star in stars:
-		draw_circle(star.position, star.size, STAR_COLOR, false)
-	
+	for star in $Starmap.get_children():
+		draw_circle( star.position, star.size, STAR_COLOR, false )
+	#for s in star_lanes:
+		#draw_line(stars[s.s0].position, stars[s.s1].position, STARLANE_COLOR, 0.25, true)
+	#for star in stars:
+		#draw_circle(star.position, star.size, STAR_COLOR, false)
+		
 	#draw_string(default_font, Vector2.ZERO, "0,0", HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
 	#draw_string(default_font, Vector2(-0.5 * MAP_WIDTH, -0.5 * MAP_HEIGHT), ",0", HORIZONTAL_ALIGNMENT_LEFT, -1, default_font_size)
+	pass
 
 func generate_stars():
-	# generate star systems
 	for i in range(STAR_COUNT):
-		var pos = Vector2(randi() % MAP_WIDTH - (0.5 * MAP_WIDTH), randi() % MAP_HEIGHT - (0.5 * MAP_HEIGHT))
-		var s = randf() * 11 + 3  # Random size between 3 and 12
-		var br = randf() * 0.5 + 0.5  # Random brightness between 0.5 and 1
-		stars.append({
-			"idx": i,
-			"position": pos, 
-			"size": s, 
-			"brightness": br,
-			"distance_to_star": {}, # distance to all other stars by star index
-			"stars_by_distance": [], # list of stars by distance to this star, ascending
-		})
-	# generate star lanes to connect the stars
+		var star = star_scene.instantiate()
+		star.position = Vector2( randi() % MAP_WIDTH - (0.5 * MAP_WIDTH), randi() % MAP_HEIGHT - (0.5 * MAP_HEIGHT) )
+		star.size = randf() * 11 + 3  # Random size between 3 and 12
+		star.brightness = randf() * 0.5 + 0.5  # Random brightness between 0.5 and 1
+		star.distance_to_star = {} # distance to all other stars by star index
+		star.stars_by_distance = [] # list of stars by distance to this star, ascending
+		$Starmap.add_child(star)
+
 	for i in range(STAR_COUNT):
 		for j in range(STAR_COUNT):
-			stars[i].distance_to_star[j] = stars[i].position.distance_to(stars[j].position)
+			$Starmap.get_child(i).distance_to_star[j] = $Starmap.get_child(i).position.distance_to( $Starmap.get_child(j).position )
 	for i in range(STAR_COUNT):
-		#stars[i].stars_by_distance = []
 		for j in range(STAR_COUNT):
-			stars[i].stars_by_distance.append({
+			$Starmap.get_child(i).stars_by_distance.append({
 				"star_idx": j,
-				"dist": stars[j].distance_to_star[i]
+				"dist": $Starmap.get_child(j).distance_to_star[i]
 			})
-			stars[i].stars_by_distance.sort_custom(compare_star_distance)
+			$Starmap.get_child(i).stars_by_distance.sort_custom(compare_star_distance)
 	for i in range(STARLANE_COUNT):
 		var s0_idx = randi() % STAR_COUNT
 		star_lanes.append({
 			"s0": s0_idx,
-			"s1": stars[s0_idx].stars_by_distance[1].star_idx, # first star is always self with distance 0, closest neighbor should be position 1
+			"s1": $Starmap.get_child(s0_idx).stars_by_distance[1].star_idx, # first star is always self with distance 0, closest neighbor should be position 1
 		})
-
+	
+	pass
+	
 func compare_star_distance(s0, s1):
 	return (s1.dist - s0.dist) > 0
